46daddcaf68b307a210178d97ae0dfb5c1eab666
Park Jae Hong <105151063+PHJ2000@users.noreply.github.com>
2025-03-28 22:39:21 +0900

    feat(programmers): 두 수의 합 풀이 완료, Closes #15

---
+++ programmers/두 수의 합.py
@@ -0,0 +1,12 @@
+"""
+사이트: 프로그래머스
+문제 이름: [두 수의 합](https://school.programmers.co.kr/learn/courses/30/lessons/181846)
+
+풀이 접근 방법:
+- str과 int를 이용하여 풀었다.
+
+느낀점:
+- 간단했다
+"""
+def solution(a, b):
+    return str(int(a)+int(b))
