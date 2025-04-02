910aea30fa0aa5bc5c402a1c327d8fa45f870fdc
Park Jae Hong <105151063+PHJ2000@users.noreply.github.com>
2025-04-02 12:18:06 +0900

    Update 두 정수 사이의 합.py

--- programmers/두 정수 사이의 합.py
+++ programmers/두 정수 사이의 합.py
@@ -13,3 +13,20 @@ def solution(a, b):
         return sum(i for i in range(b,a+1))
     else:
         return sum(i for i in range(a,b+1))
+    
+"""
+좋은 풀이
+def adder(a, b):
+    if a > b:
+        a, b = b, a
+    return sum(range(a, b + 1))
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print( adder(3, 5))
+
+def adder(a, b):
+    return (abs(a-b)+1)*(a+b)//2
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print( adder(3, 5))
+"""
